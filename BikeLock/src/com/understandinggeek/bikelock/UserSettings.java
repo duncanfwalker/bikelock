d82652283f719f8c8233aaf8055187ac397d0546
duncanwalker <duncfw@gmail.com>
2013-05-14 00:56:17 +0100

    Auto reply message text setting and sms sending

---
+++ BikeLock/src/com/understandinggeek/bikelock/UserSettings.java
@@ -0,0 +1,14 @@
+package com.understandinggeek.bikelock;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserSettings extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        addPreferencesFromResource(R.xml.settings);
+ 
+    }
+}
